0f99197cc0961945a8a67143156475ea82be44b5
choerauf <christian.hoerauf@inserteffect.com>
2014-08-29 17:16:27 +0200

    safeTail implemented

--- 04_Functional_Programming/ExcercisesP84Spec.hs
+++ 04_Functional_Programming/ExcercisesP84Spec.hs
@@ -7,6 +7,7 @@ import ExercisesP84
 main :: IO ()
 main = hspec $ do
   describe "1. Write your own 'safe' definitions of the standard partial  list functions, but make sure they never fail." $ do
+
    describe "safeHead" $ do
     it "returns the first value of a given list" $ do
       safeHead [1,2,3] `shouldBe` Just 1
@@ -14,3 +15,12 @@ main = hspec $ do
     it "returns Nothing if empty list is given" $ do
       safeHead ([]::[Int]) `shouldBe` Nothing
 
+   describe "safeTail" $ do
+    it "returns all but the first value of a given list" $ do
+      safeTail [1,2,3] `shouldBe` Just [2,3]
+
+    it "returns an empty List, if list contains single item" $do
+      safeTail [1] `shouldBe` Just []
+
+    it "return Nothing if list is empty" $ do
+      safeTail ([]::[Int]) `shouldBe` Nothing
--- 04_Functional_Programming/ExercisesP84.hs
+++ 04_Functional_Programming/ExercisesP84.hs
@@ -2,3 +2,7 @@ module ExercisesP84 where
 safeHead :: [a] -> Maybe a
 safeHead (x:xs) = Just(x)
 safeHead [] = Nothing
+
+safeTail :: [a] -> Maybe [a]
+safeTail (x:xs) = Just xs
+safeTail [] = Nothing
