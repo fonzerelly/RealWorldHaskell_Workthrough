363efb2c9e7b03b3157e65c6338892b25358a0b9
fonzerelly <ChristianHoerauf@web.de>
2014-09-27 23:06:56 +0200

    Init Implementation of SimpleJSON

---
+++ 05_Writing_a_Library_Working_with_JSON_Data/SimpleJSON.hs
@@ -0,0 +1,8 @@
+-- file: ch05/SimpleJSON.hs
+data JValue = JString String
+            | JNumber Double
+            | JBool Bool
+            | JNull
+            | JObject [(String, JValue)]
+            | JArray [JValue]
+              deriving (Eq, Ord, Show)
