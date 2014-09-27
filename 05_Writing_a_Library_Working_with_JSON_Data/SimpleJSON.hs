2e2ac7288185cdc03592098d70796562d26231ab
fonzerelly <ChristianHoerauf@web.de>
2014-09-28 00:39:53 +0200

    PutJSON implemented

--- 05_Writing_a_Library_Working_with_JSON_Data/SimpleJSON.hs
+++ 05_Writing_a_Library_Working_with_JSON_Data/SimpleJSON.hs
@@ -1,4 +1,17 @@
 -- file: ch05/SimpleJSON.hs
+
+module SimpleJSON
+  (
+    JValue(..)
+  , getString
+  , getInt
+  , getDouble
+  , getBool
+  , getObject
+  , getArray
+  , isNull
+  ) where
+
 data JValue = JString String
             | JNumber Double
             | JBool Bool
@@ -6,3 +19,32 @@ data JValue = JString String
             | JObject [(String, JValue)]
             | JArray [JValue]
               deriving (Eq, Ord, Show)
+
+getString :: JValue -> Maybe String
+getString (JString s) = Just s
+getString _           = Nothing
+
+getInt :: JValue -> Maybe Int
+getInt (JNumber i)
+  | floor i == ceiling i = Just (truncate i)
+  | otherwise            = Nothing
+getInt _           = Nothing
+
+getDouble :: JValue -> Maybe Double
+getDouble (JNumber d) = Just d
+getDouble _           = Nothing
+
+getBool :: JValue -> Maybe Bool
+getBool (JBool b) = Just b
+getBool _           = Nothing
+
+getObject :: JValue -> Maybe [(String, JValue)]
+getObject (JObject o) = Just o
+getObject _           = Nothing
+
+getArray :: JValue -> Maybe [ JValue ]
+getArray (JArray a) = Just a
+getArray _          = Nothing
+
+isNull v = v == JNull
+
